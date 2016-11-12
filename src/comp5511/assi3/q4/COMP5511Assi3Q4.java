55e0dcacd67be5b533efc7fa08218b8ffe5c9d30
Rodrigo <rpessoa.mobile@gmail.com>
2016-11-12 17:32:23 -0500

    Included output and edited format
    I added 2 new outputs. One for the Original sorted list and one for the reverse list.
    And edited a little bit the format.

--- src/comp5511/assi3/q4/COMP5511Assi3Q4.java
+++ src/comp5511/assi3/q4/COMP5511Assi3Q4.java
@@ -5,23 +5,23 @@ public class COMP5511Assi3Q4 {
 
     
     public static void main(String[] args) {
-      
-      LinkedList Data = new LinkedList();  
-      //insert data in the List
-      int x= 472945921;
-      for(int i=0; i < 100; i++){
-        Data.add(x);
-        x += 6631245;
-      }
-      
-  
-      int n = Data.size();
-     
-     
+        
+        LinkedList Data = new LinkedList();  
+        //insert data in the List
+        int x= 472945921;
+        for(int i=0; i < 100; i++) {
+            Data.add(x);
+            x += 6631245;
+        }
+        
+        int n = Data.size();
+         
         //print the initial sorted list
-         for (int z=0; z<n; z++){
-         System.out.print("ISBN: " + Data.get(z)+ "\n");}
-         System.out.println();
+        System.out.println("Initial Sorted List"); 
+        for (int z=0; z<n; z++) {
+            System.out.print("ISBN: " + Data.get(z)+ "\n");
+        }
+            System.out.println();
          
         //possibility to simply use the API below 
         //Collections.reverse(Data);
@@ -32,20 +32,18 @@ public class COMP5511Assi3Q4 {
             //2 Add it where it belongs
             //3 Delete value from location 0
             
-            //(1)Take data in value 0 and (2)add at location (n-u)
-            Data.add(n-u,Data.get(0));
-            //(3)Then delete value 0
-            Data.remove();
+        //(1)Take data in value 0 and (2)add at location (n-u)
+        Data.add(n-u,Data.get(0));
+        //(3)Then delete value 0
+        Data.remove();
         }
      
-     //Print reverse sorted list
-     for (int z=0; z<n; z++){
-         System.out.print("ISBN: "+Data.get(z) + "\n");}
+        //Print reverse sorted list
+        System.out.println("Reversed Sorted List");
+        for (int z=0; z<n; z++) {
+            System.out.print("ISBN: "+Data.get(z) + "\n");
+        }
     }
-     
-   
-        
-    
 }
     
 
